37f4e9aea4c4475ef91ad46ea1e4e0693e1a4bad
Omar Brikaa <brikaaomar@gmail.com>
2024-05-19 15:39:12 +0300

    bug fixes

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import cv2
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from threading import Thread
+import shutil
 
 dataset_dir = "./dataset/"
 dirs = [
@@ -26,7 +27,6 @@ def preprocess_dir(dir_path, preprocessed_dir_path):
                 os.path.join(preprocessed_dir_path, file_name), image_normalized
             )
             features.append(image_normalized)
-            target_names.append(dir_name)
 
         except:
             print(file_path + " is bad")
@@ -37,9 +37,12 @@ threads = []
 print("Preprocessing")
 for dir_name in dirs:
     dir_path = os.path.join(dataset_dir, dir_name)
-    preprocessed_dir_path = os.path.join(dataset_dir, dir_name + "-preprocessed")
-    if os.path.exists(preprocessed_dir_path):
+    if dir_path.endswith("-preprocessed"):
         continue
+    preprocessed_dir_path = os.path.join(dataset_dir, dir_name + "-preprocessed")
+    target_names.append(dir_name)
+    if os.path.exists(preprocessed_dir_path):
+        shutil.rmtree(preprocessed_dir_path)
     os.mkdir(preprocessed_dir_path)
     t = Thread(
         target=preprocess_dir,
@@ -55,3 +58,6 @@ features = np.array(features)
 
 label_encoder = LabelEncoder()
 targets = label_encoder.fit_transform(target_names)
+print(target_names)
+print(targets)
+print(len(features))
